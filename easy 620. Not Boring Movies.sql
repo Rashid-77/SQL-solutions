b57e272ed5c00f9eb88d4b9e340459710cd0c70c
Rashid <34908431+Rashid-77@users.noreply.github.com>
2022-09-09 07:11:14 +0500

    Update easy 620. Not Boring Movies.sql

--- easy 620. Not Boring Movies.sql
+++ easy 620. Not Boring Movies.sql
@@ -39,3 +39,7 @@
 #  We have three movies with odd-numbered IDs: 1, 3, and 5. The movie with ID = 3 is boring so we do not include it in the answer.
 
 
+SELECT *
+FROM Cinema
+WHERE description <> "boring" AND MOD(id, 2) = 1
+ORDER BY rating DESC
